3b3a7ee8083ef53f98719e42915c9580c33bc140
cetineremre <emre.cetiner@live.com>
2016-10-18 13:22:12 +0300

    Changed main function definition, solved compile error

--- server.py
+++ server.py
@@ -14,26 +14,31 @@ from handlers import site
 
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object('settings')
-    app.register_blueprint(site)
+	app = Flask(__name__)
+	app.config.from_object('settings')
+	app.register_blueprint(site)
 
-    app.store = Store()
-    app.store.add_twit(Twit('Mr First Tweet'	, 'Just so you don\'t get tired we posted your first tweet'))
+	app.store = Store()
+	app.store.add_twit(Twit('Mr First Tweet', 'Just so you don\'t get tired we posted your first tweet'))
 
-    app.messageList = MessageList()
-    app.messageList.add_message(Message('Emre Cetiner', 'Serkan Bekir', 'hello', sent = False))
-    app.messageList.add_message(Message('Yusuf Ekiz', 'Serkan Bekir', 'good morning', sent = True))
-    app.messageList.add_message(Message('Mert Kurtcan', 'Serkan Bekir', 'hi!'))
-    app.messageList.add_message(Message('Cem Karagoz', 'Serkan Bekir', 'good afternoon'))
+	app.messageList = MessageList()
+	app.messageList.add_message(Message('Emre Cetiner', 'Serkan Bekir', 'hello', sent = False))
+	app.messageList.add_message(Message('Yusuf Ekiz', 'Serkan Bekir', 'good morning', sent = True))
+	app.messageList.add_message(Message('Mert Kurtcan', 'Serkan Bekir', 'hi!'))
+	app.messageList.add_message(Message('Cem Karagoz', 'Serkan Bekir', 'good afternoon'))
 
-    return app
+	return app
+
+
+def main():
+	app = create_app()
+	VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
+	if VCAP_APP_PORT is not None:
+		port, debug = int(VCAP_APP_PORT), False
+	else:
+		port, debug = 5000, True
+	app.run(host='0.0.0.0', port=port, debug=debug)
 
 
 if __name__ == '__main__':
-    VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
-    if VCAP_APP_PORT is not None:
-    	port, debug = int(VCAP_APP_PORT), False
-    else:
-    	port, debug = 5000, True
- 	app.run(host='0.0.0.0', port=port, debug=debug)
+	main()
