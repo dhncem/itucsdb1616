ad3606fbeeb34a3370c4fe29b2a4ad9ea9a8c46f
cetineremre <emre.cetiner@live.com>
2016-10-27 00:24:01 +0300

    set up elephantsql connection

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import Flask
 import datetime
 import os
+import json
+import re
+
+from flask import Flask
 from handlers import site
 from flask import Blueprint, render_template
 from store import Store
@@ -9,11 +12,21 @@ from message import Message
 from messageList import MessageList
 from list import List
 from listoflist import ListOfLists
-
 from flask import Flask
-
 from handlers import site
 
+
+
+
+def get_elephantsql_dsn(vcap_services):
+	"""Returns the data source name for ElephantSQL."""
+	parsed = json.loads(vcap_services)
+	uri = parsed["elephantsql"][0]["credentials"]["uri"]
+	match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+	user, password, host, _, port, dbname = match.groups()
+	dsn = """user='{}' password='{}' host='{}' port={}
+			dbname='{}'""".format(user, password, host, port, dbname)
+	return dsn
 
 def create_app():
 	app = Flask(__name__)
@@ -46,6 +59,13 @@ def main():
 		port, debug = int(VCAP_APP_PORT), False
 	else:
 		port, debug = 5000, True
+
+	VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+	if VCAP_SERVICES is not None:
+		app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+	else:
+		app.config['dsn'] = """user='vagrant' password='vagrant' host='localhost' port=54321 dbname='itucsdb'"""
+
 	app.run(host='0.0.0.0', port=port, debug=debug)
 
 
