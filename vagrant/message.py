949a02b69253b4acc5db7f4b1c0d119bffac4c87
cetineremre <emre.cetiner@live.com>
2016-10-27 19:37:27 +0300

    applied bootstrap to layout applied layout to sign in page fixed appereance problems on some pages removed unrelated file from vagrant folder

--- vagrant/message.py
+++
@@ -1,7 +0,0 @@
-class Message:
-    def __init__(self, sender, reciever, content):
-        self.sender = sender
-        self.reciever = reciever
-        self.content = content
-        
-        
