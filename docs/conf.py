7b0feb5be232eb129cce4f9ca026e7bf759345f2
cetineremre <emre.cetiner@live.com>
2016-12-30 13:02:18 +0300

    fixed mistakes

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'itucsdb1616'
-copyright = '2016, itucsdb1616'
+copyright = '2016, Team Name'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -198,7 +198,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'itucsdb1616.tex', 'itucsdb1616 Documentation',
-   'Twitter', 'manual'),
+   'itucsdb1616', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -228,7 +228,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'itucsdb1616', 'itucsdb1616 Documentation',
-     ['Twitter'], 1)
+     ['Team Name'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -242,7 +242,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'itucsdb1616', 'itucsdb1616 Documentation',
-   'Twitter', 'itucsdb1616', 'One line description of project.',
+   'Team Name', 'itucsdb1616', 'One line description of project.',
    'Miscellaneous'),
 ]
 
